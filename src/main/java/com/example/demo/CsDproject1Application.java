93001dce7e478ff3feace46423b4275cccb58e08
RaquelP <r.pena@campus.fct.unl.pt>
2022-04-01 17:14:24 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/CsDproject1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsDproject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsDproject1Application.class, args);
+	}
+
+}
